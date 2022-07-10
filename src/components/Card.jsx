921291c71d7897ba27be1db22c68ba5f13f24f50
Priyanshu Bharti <46714242+pseudoslayr@users.noreply.github.com>
2022-07-10 19:26:53 +0530

    Bug Fixes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card({ title, desc, src, alt, list, grad, sh }) {
   
 
   return (
-    <div data-aos="fade-in" data-aos-delay="300" className={`shadow-xl md:hover:scale-105 bg-slate-50 md:hover:bg-white transition-transform rounded-lg overflow-hidden flex flex-col mb-5 md:mb-0`}>
+    <div data-aos="fade-in" data-aos-delay="300" className={`shadow-xl md:hover:scale-105 bg-slate-50 transition md:hover:bg-white rounded-lg overflow-hidden flex flex-col mb-5 md:mb-0`}>
       <div className={`py-8 bg-gradient-to-br ${grad}`}>
         <img src={"./img/" + src} alt={alt} className="glyph" />
       </div>
