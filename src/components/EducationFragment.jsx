a2471e42103545d21ab628e39be8582e42da3092
Priyanshu Bharti <46714242+pseudoslayr@users.noreply.github.com>
2022-07-10 19:22:43 +0530

    Fixed Minor issues

--- src/components/EducationFragment.jsx
+++ src/components/EducationFragment.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function EducationFragment({sno, title, org, year}) {
   return (
     <>
-      <div data-aos="fade-in" data-aos-delay="500" className="flex shadow-lg shadow-slate-200 rounded-2xl md:hover:scale-105 transition bg-slate-50 p-4">
+      <div data-aos="fade-in" data-aos-delay="500" className="flex shadow-lg shadow-slate-200 rounded-2xl md:hover:bg-white md:hover:scale-105 transition bg-slate-50  p-4">
         <div className="text-transparent bg-clip-text bg-gradient-to-br from-teal-500 to-cyan-400 font-bold text-6xl mr-4">
           {sno}
         </div>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.unregister();
